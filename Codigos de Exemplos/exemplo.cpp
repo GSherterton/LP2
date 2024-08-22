d2a2fdb319edffe74a8428610502165dcf2a1b4d
GSHerterton <gsherterton@gmail.com>
2024-08-22 15:33:43 -0300

    Reorganizando os arquivos

---
+++ Codigos de Exemplos/exemplo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    string str = argv[1];
+
+    cout << "Rodando " << str << endl;
+
+    return 0;
+}
